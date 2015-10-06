a33c7436af19704e57bc6fc6c1ef6bc5bf146081
dazzacurdan <pasterispaolo@gmail.com>
2015-10-06 07:26:15 -0700

    Introduced file base mission input and an help function for the user

--- MarsLanding/src/MarsMission.java
+++ MarsLanding/src/MarsMission.java
@@ -1,21 +1,81 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import java.util.*;
-/*TODO
- * 1) Improve INPUT parsing
- * 2) Add handling of map size
- * */
+
 public class MarsMission {
 	public static void main( String[] args){
-		Vector<Rover> rovers = new Vector<Rover>();
-		rovers.add(new Rover(1,2,Directions.NORTH));
-		rovers.lastElement().setMission("LMLMLMLMM");
-		rovers.add(new Rover(3,3,Directions.EAST));
-		rovers.lastElement().setMission("MMRMMRMRRM");
 		
-		Iterator<Rover> it = rovers.iterator();
-		while( it.hasNext() ){
-			Rover obj=it.next();
-			if(obj.run())
-				obj.endMission();
+		if( args.length != 1 ){
+			help();
+		}else{
+			
+			Vector<String> missionInformation = parseMissionFile(args[0]);
+			if( missionInformation != null ){
+				
+				Iterator<String> mission_it = missionInformation.iterator();
+				String[] mapSize = mission_it.next().split(" ");
+				Vector<Rover> rovers = new Vector<Rover>();
+				
+				while( mission_it.hasNext() ){
+					String[] initialPose = mission_it.next().split(" ");
+					rovers.add(new Rover( Integer.parseInt(initialPose[0]),
+										  Integer.parseInt(initialPose[1]),
+										  Directions.getDirectionForCode(initialPose[2].charAt(0)) ));
+					rovers.lastElement().setMission(mission_it.next());
+				}
+								
+				Iterator<Rover> it = rovers.iterator();
+				while( it.hasNext() ){
+					Rover obj=it.next();
+					if(obj.run())
+						obj.endMission();
+				}
+			}else{
+				System.out.println("\t\t\t.:Invalid mission:.");
+				help();
+			}
 		}
+	}
+
+	private static void help() {
+		// TODO Auto-generated method stub
+		System.out.println("Please provide information for the new mission ");
+		System.out.println("Organize a misson file like: mission.txt ");
+		
+		System.out.println("\n5 5");
+		System.out.println("1 2 N");
+		System.out.println("LMLMLMLMM");
+		System.out.println("3 3 E");
+		System.out.println("MMRMMRMRRM \n");
+		
+		System.out.println("The first line of input is the upper-right coordinates of the plateau, the lower-left coordinates are assumed to be 0,0");
+		System.out.println("The rest of the input is information pertaining to the rovers that have been deployed.\nEach rover has two lines of input.\nThe first line gives the rover’s position, and the second line is a series of instructions telling the rover how to explore the plateau.");
+		System.out.println("Finally call MarsMission.java mission.txt");
+	}
+	private static Vector<String> parseMissionFile(String fileName){
+		Vector<String> missionInformation = new Vector<String>();
+		try {
+			InputStream in = new FileInputStream(fileName);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			String strLine;
+			//Read File Line By Line
+			while ((strLine = br.readLine()) != null)   {
+				missionInformation.add(strLine);
+			}
+			//Close the input stream
+			in.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		if( missionInformation.size() % 2 == 0  ){
+			missionInformation.clear();
+			return null;
+		}
+		return missionInformation;
 	}
 }
